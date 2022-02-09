ab417d09860821db88e48461269948eb90864e32
ThePhantomD3v <fordjourbobiem@gmail.com>
2022-02-09 01:23:41 +0000

    new product controller logic

---
+++ Restaurant_Bck/helpers/schema_validation.helper.js
@@ -0,0 +1,17 @@
+const joi = require('joi')
+
+const productSchema = joi.object({
+	name: joi.string().min(3).required(),
+	image: joi.string().required(),
+	food_type: joi.string().required(),
+	price: joi.number().required(),
+	description: joi.string().required(),
+})
+
+const userSchema = joi.object({
+	username: joi.string().min(3).require(),
+	email: joi.string().email().lowercase(),
+	password: joi.string().min(6).patten(new RegExp('^[a-zA-Z0-9]{3-30}$')),
+})
+
+module.exports = { productSchema, userSchema }
