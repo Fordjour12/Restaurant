9f78531e5233838d760e16e1bb4e3a0b1a8a73a4
McCamble Kofi Fordjour Bobie <fordjourbobiem@gmail.com>
2022-03-13 04:46:57 +0000

    popular foods swiper completion

--- Restaurants_Fnt/src/components/Homepage.jsx
+++ Restaurants_Fnt/src/components/Homepage.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+// swiper js
+import { Swiper, SwiperSlide } from 'swiper/react'
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+import { Autoplay, Pagination } from 'swiper'
+// swiper js end
 import Menu from '../assets/images/Menu'
 import Notification from '../assets/images/Notification'
 import Wishlist from '../assets/images/Wishlist'
 import Search from '../assets/images/Search'
-import img1 from '../assets/images/pizza-2.png'
-import img2 from '../assets/images/soup.png'
-import img3 from '../assets/images/juice.png'
-import img4 from '../assets/images/spaghetti-2.png'
 import hero from '../assets/images/food.jpg'
 import { categories } from '../data/categories'
 import { foods } from '../data/foods'
@@ -94,32 +97,74 @@ const Homepage = () => {
 						<label htmlFor='foods' className='popular-food-title'>
 							Popular Foods
 						</label>
+
 						<ul className='popular-food-lists'>
-							{foods.map((food, idx) => {
-								const { img, name,price } = food
-								return (
-									<li
-										key={idx}
-										className='popular-food-lists-list'
-									>
-										<img
-											src={img}
-											className='popular-food-lists-list-img'
-											alt={name}
-										/>
-										<p className='popular-food-lists-list-title'>
-											{name}
-										</p>
-										<p className='popular-food-lists-list-price'>
-											{price}
-										</p>
-									</li>
-								)
-							})}
+							<Swiper
+								loop={true}
+								pagination={{ clickable: true }}
+								autoplay={{
+									delay: 2500,
+									disableOnInteraction: false,
+								}}
+								modules={[Pagination, Autoplay]}
+								className='mySwiper'
+							>
+								{foods.map((food, idx) => {
+									const { img, name, price } = food
+									return (
+										<>
+											<SwiperSlide>
+												<li
+													key={idx}
+													className='popular-food-lists-list'
+												>
+													<img
+														src={img}
+														className='popular-food-lists-list-img'
+														alt={name}
+													/>
+													<p className='popular-food-lists-list-title'>
+														{name}
+													</p>
+													<p className='popular-food-lists-list-price'>
+														{price}
+													</p>
+												</li>
+											</SwiperSlide>
+										</>
+									)
+								})}
+							</Swiper>
 						</ul>
 					</div>
 					<div className='popular-drinks'>
 						<label htmlFor='Drinks'>Popular Drinks</label>
+						{/* <>
+							<Swiper
+								spaceBetween={30}
+								centeredSlides={true}
+								autoplay={{
+									delay: 2500,
+									disableOnInteraction: false,
+								}}
+								pagination={{
+									clickable: true,
+								}}
+								navigation={true}
+								modules={[Autoplay, Pagination, Navigation]}
+								className='mySwiper'
+							>
+								<SwiperSlide>Slide 1</SwiperSlide>
+								<SwiperSlide>Slide 2</SwiperSlide>
+								<SwiperSlide>Slide 3</SwiperSlide>
+								<SwiperSlide>Slide 4</SwiperSlide>
+								<SwiperSlide>Slide 5</SwiperSlide>
+								<SwiperSlide>Slide 6</SwiperSlide>
+								<SwiperSlide>Slide 7</SwiperSlide>
+								<SwiperSlide>Slide 8</SwiperSlide>
+								<SwiperSlide>Slide 9</SwiperSlide>
+							</Swiper>
+						</> */}
 					</div>
 					<div className='popular-pizza'>
 						<label htmlFor='Pizza'>Popular Pizza</label>
